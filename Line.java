3fc87f2b7d1141aa2abe298e08d96238e3bfc7ed
shulinlin <syang28@stevens.edu>
2017-03-17 08:56:15 +0800

    homework 6

---
+++ Line.java
@@ -0,0 +1,18 @@
+
+import java.awt.Graphics;
+
+public class Line extends Shape{
+
+	private int x2,y2;
+	public Line(int x1, int y1,int x2,int y2) {
+		super(x1, y1);
+		this.x2 = x2;
+		this.y2 = y2;
+		// TODO Auto-generated constructor stub
+	}
+	public void draw(Graphics g){
+		g.drawLine(x, y, x2, y2);
+	}
+	
+}
+
