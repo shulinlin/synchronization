4a85874b0ece2cad9c1ffe91512be06da127eae5
shulinlin <syang28@stevens.edu>
2017-03-27 04:42:35 +0800

    finished hashmap using linear chaining

--- Hashmap.java
+++ Hashmap.java
@@ -2,71 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Hashmap {
-	//  private String[] keys;
-	//	private int values[];
-
 	private static class Node {
 		String key;
-		int val;
 		Node next;
-		Node(String k, int v) { key = k; val = v; }
+		Node(String k) { key = k;  }
 	}
 	private Node[] table;
 	private int used;
 	private int[] hist;
 	private final int hash(String word) {
-		int sum = word.length();
+		int hash = word.length();
 		for (int i = 0; i < word.length(); i++)
-			sum = sum << 13 + sum << 3 + word.charAt(i) + sum >> 17;
-		return sum & (table.length-1);
-	}
-	private void grow() {
-		// double in size, reinsert all elements
+			hash = 31*hash + word.charAt(i);
+		return Math.abs(hash) % (table.length);
 	}
 	public Hashmap(int initialSize) {
 		table = new Node[initialSize];
 		used = 0;
-		hist = new int[12];
+		hist = new int[initialSize];
 	}
 
 	public void load(String filename) throws Exception {
 		FileReader r = new FileReader(filename);
 		Scanner s = new Scanner(r);
-		int count = 0;
 		while (s.hasNext()) {
 			String word = s.next();
-			add(word, count++);
+			add(word);
 		}
 		s.close();
 	}
-
-	public void add(String word, int v) {
-		
+	public void add(String word) {
 		int pos = hash(word);
-	//	System.out.println(pos);
 		Node current = table[pos];
-		Node temp = current;
-		int count = 1;
-		while(temp!=null){
-			current = temp;
-			temp = temp.next;
-			current.next = temp;
-		}
-//		while (table[pos] != null) {
-//			if (table[pos].key.equals(word)) {
-//				table[pos].val = v;
-//				if (count > 11)
-//					count = 11;
-//				hist[count]++;
-//				return;
-//			}
-			temp = new Node(word,v);
-//		}
-		// guaranteed that table[pos].key == null
+		Node temp = new Node(word);
+		temp.next = table[pos];
+		table[pos] = temp;
 		used++;
-		if(count>11)
-			count=11;
-		hist[count]++;
+		hist[pos]++;
 	}
 
 	public boolean contains(String word) {
@@ -76,28 +48,24 @@ public class Hashmap {
 			if (temp.key.equals(word))
 				return true;
 			temp = temp.next;
-			
+
 		}
 		return false;
 	}
 	public void printHist() {
-		for (int i = 1; i < 12; i++) {
+		for (int i = 0; i < hist.length; i++) {
 			System.out.println(i + "\t" + hist[i]);
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		Hashmap m = new Hashmap(1000);
+		Hashmap m = new Hashmap(26);
 		m.load("dict.txt");
-		//	m.load("c++keywords.dat");
-		/*
-				Integer v = m.get("cat");
-		if (v != nullptr) {
-			cout << v.intValue() << '\n';
+		File f = new File("hw8.dat");
+		Scanner s = new Scanner(f);
+		while(s.hasNext()){
+			System.out.println(m.contains(s.next()));
 		}
-		 */
-		System.out.println(m.contains("hello"));
-		System.out.println(m.contains("aa"));
 		m.printHist();
 	}
 }
