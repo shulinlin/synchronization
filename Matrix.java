059f5bb3a0b3cfa2e5cfbcb6b9c0ed50315657e0
shulinlin <syang28@stevens.edu>
2017-04-18 20:36:25 +0800

    matrix

--- Matrix.java
+++ Matrix.java
@@ -1,13 +1,13 @@
 
 public class Matrix {
 
-	private int[] m;
+	private double[] m;
 	private int n;
 	Matrix(int n){
 		this.n = n;
-		m = new int[n*n];
+		m = new double[n*n];
 	}
-	Matrix(int[] a,int n){
+	Matrix(double[] a,int n){
 		m=a;
 		this.n = n;
 	}
@@ -22,19 +22,44 @@ public class Matrix {
 	}
 	public Matrix mul(Matrix m1){
 		Matrix mn = new Matrix(m1.n);
-		for(int i=0;i<m1.n;i++){
-			for(int j=0;j<m1.n;j++){
-				mn.m[j*n+i]+=m[j*n+i]*m1.m[j*n+i];
+		int temp=0;
+		for(int count=0;count<m1.n;count++){
+			for(int i=0;i<m1.n;i++){
+				for(int j=0;j<m1.n;j++){
+					temp+=m[j+count*n]*m1.m[j*n+i];	
+				}
+				mn.m[i+count*n] = temp;
+				temp = 0;
 			}
 		}
+
 		return mn;
 	}
+	public Matrix inverse(){
+		Matrix iden = new Matrix(n);
+		for(int i=0;i<n;i++){
+			iden.m[i*n+i] = 1;
+		}
+		for(int i=0;i<n-1;i++){
+			for(int j=i+1;j<n;j++){
+				double pivot = m[i*n+i]/m[j*n+i];
+				for(int col=0;col<n;col++){
+					m[j*n+col]=m[j*n+col]*pivot-m[i*n+col];
+					iden.m[j*n+col] = iden.m[j*n+col]*pivot-iden.m[i*n+col];
+				}
+			}
+		}
+		return iden;
+	}
 	public static void main(String[] args){
-		int[] a = {0,1,2,3};
-		int[] b = {1,2,3,4};
+		double[] a = {0,1,2,3};
+		double[] b = {1,2,3,4};
 		Matrix m = new Matrix(a,2);
+		
 		Matrix m1 = new Matrix(b,2);
-		Matrix m2 = m.mul(m1);
+		m1.disp();
+		System.out.println();
+		Matrix m2 = m1.inverse();
 		m2.disp();
 	}
 }
